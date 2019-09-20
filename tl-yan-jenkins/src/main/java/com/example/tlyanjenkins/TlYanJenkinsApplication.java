4e8014cb93685d2b003473122fec6009135aecc4
yan shuai <704627050@qq.com>
2019-09-20 17:14:44 +0800

    jenkins

---
+++ tl-yan-jenkins/src/main/java/com/example/tlyanjenkins/TlYanJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tlyanjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlYanJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlYanJenkinsApplication.class, args);
+	}
+
+}
